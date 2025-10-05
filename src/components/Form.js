ac801f4f3b92a8f74fb073e1982a0b70bfc4a287
Aryan Khaire <aryan.khaire001@gmail.com>
2025-10-05 23:33:09 +0530

    Done

---
+++ src/components/Form.js
@@ -0,0 +1,47 @@
+import {useState} from "react"
+
+export default function Form(){
+    const [name,setName] = useState({
+        fname: "",
+        lname: "",
+        submittedFName: "",
+        submittedLName: ""
+    })
+
+    const handleChange = (e) => {
+        setName({
+            ...name,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setName({
+            ...name,
+            submittedFName : name.fname,
+            submittedLName : name.lname,
+        })
+    }
+
+    return(
+        <>
+            <h1>Full Name Display</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlfor="fname">First Name: </label>
+                <input required type="text" name="fname" id="fname" onChange={handleChange}/> <br/>
+
+                <label htmlfor="lname">Last Name: </label>
+                <input required type="text" name="lname" id="lname" onChange={handleChange}/> <br/>
+
+                <input type="submit" name="submit" id="submit"/> <br/>
+            </form>
+
+            <div className="display-name">{
+                (name.submittedFName !== "" && name.submittedLName !== "") ? (`Full Name: ${name.submittedFName} ${name.submittedLName}`) : (``)     
+            }</div>
+        </>
+    )
+} 
